392bbbebb355aa07f7dc73f946da8a615cc78154
ws han <pyanfield@gmail.com>
2011-08-03 23:30:19 +0800

    chapter 9,p318

---
+++ Nav/Nav/MoveMeController.h
@@ -0,0 +1,13 @@
+//
+//  MoveMeController.h
+//  Nav
+//
+//  Created by guo gloria on 11-8-3.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MoveMeController : NSObject
+
+@end
---
+++ Nav/Nav/MoveMeController.m
@@ -0,0 +1,23 @@
+//
+//  MoveMeController.m
+//  Nav
+//
+//  Created by guo gloria on 11-8-3.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MoveMeController.h"
+
+@implementation MoveMeController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
