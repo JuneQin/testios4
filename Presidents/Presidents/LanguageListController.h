215c6224f291ce0e0cd904c00ecd19bbef6864fb
ws han <pyanfield@gmail.com>
2011-08-09 22:47:08 +0800

    chapter 10,over

---
+++ Presidents/Presidents/LanguageListController.h
@@ -0,0 +1,13 @@
+//
+//  LanguageListController.h
+//  Presidents
+//
+//  Created by guo gloria on 11-8-9.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LanguageListController : UITableViewController
+
+@end
