668d7056727c9d1940238bb20ebd100aae83a4e0
ws han <pyanfield@gmail.com>
2011-08-22 23:28:07 +0800

    chapter 12,ok

---
+++ Core Data Persistence/Core Data Persistence/PersistenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersistenceViewController.h
+//  Core Data Persistence
+//
+//  Created by guo gloria on 11-8-22.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersistenceViewController : UIViewController
+
+@end
